fd3efd1c2c203c9354ed278bedbc9fb6e9cc4cb2
jjty <jye.g96@hotmail.com>
2022-05-09 18:38:04 +1000

    hello

--- test3/Core/Feed/View/FeedView.swift
+++ test3/Core/Feed/View/FeedView.swift
@@ -35,7 +35,7 @@ struct FeedView: View {
             .clipShape(Circle())
             .padding()
             .fullScreenCover(isPresented: $showNewTweetView){
-                Text("new tweet view")
+                NewTweetView()
             }
             
         }
